35bc998574372dd1ec57beb30fd02bbdd6a26115
Natalia Cancelado <nataliasst21@gmail.com>
2021-12-20 00:03:50 -0500

    Se prueba con el metodo de update

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -32,4 +32,33 @@ class ClientController extends Controller
         }
         return response()->json(['message' => '¡El campo Nombre, Cédula y Fecha son obligatorios!']);
     }
+
+    public function update(Request $request, $id) {
+
+        $name = $request->input('nombre');
+        $document = $request->input('cedula');
+        $birth_date = $request->input('fecha_nacimiento');
+
+        $client = Client::findOrFail($id);
+        $client->where('cedula', '1019221');
+        $client->update($request->all());
+        return response()->json(['message' => '¡El Cliente ha sido actualizado!']);
+
+
+        /*return redirect('usuarios');
+
+        if(!empty($name) && !empty($document) && !empty($birth_date)) {
+            $client = new Client();
+            $client->nombre = $name;
+            $client->cedula = $document;
+            $client->fecha_nacimiento = $birth_date;
+            $valid_document = Client::where('cedula', $document)->first();
+            if(!empty($valid_document['cedula'])) {
+                return response()->json(['message' => '¡El Documento no está permitido!']);
+            }
+            $client->save();
+            return response()->json(['message' => '¡El Cliente ha sido creado!']);
+        }
+        return response()->json(['message' => '¡El campo Nombre, Cédula y Fecha son obligatorios!']);*/
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,4 @@ use Illuminate\Http\Request;
 
 Route::get('clients/list/', 'ClientController@index');
 Route::post('clients/create/', 'ClientController@create');
+Route::put('clients/update/{id}', 'ClientController@update');
