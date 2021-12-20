0098d5e440e4ce8dfa8292e065f6d588b98518ab
Natalia Cancelado <nataliasst21@gmail.com>
2021-12-19 22:09:43 -0500

    Se crean los modelos y prepara estructura de tablas para realizar la migración

---
+++ database/migrations/2021_12_20_025826_create_schedules_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('asunto');
+            $table->dateTime('fecha');
+            $table->enum('estado', ['Programada', 'Realizada', 'Cancelada', 'No_Asistida']);
+            $table->foreign("id_client")
+                ->references("id")
+                ->on("clients")
+                ->onDelete("cascade")
+                ->onUpdate("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
