a2e95503ff2647c7ace1925efdcca6a73c23d18a
Natalia Cancelado <nataliasst21@gmail.com>
2021-12-19 22:51:11 -0500

    Se crea método de creación de cliente y se preparan rutas iniciales

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -12,4 +12,25 @@ class ClientController extends Controller
     {
         return response()->json(User::all());
     }
+
+    public function create(Request $request) {
+
+        $name = $request->input('nombre');
+        $document = $request->input('cedula');
+        $birth_date = $request->input('fecha_nacimiento');
+
+        if(!empty($name) && !empty($document) && !empty($birth_date)) {
+            $client = new Client();
+            $client->nombre = $name;
+            $client->cedula = $document;
+            $client->fecha_nacimiento = $birth_date;
+            $valid_document = Client::where('cedula', $document)->first();
+            if(!empty($valid_document['cedula'])) {
+                return response()->json(['msg' => '¡El Documento no está disponible!']);
+            }
+            $client->save();
+            return response()->json(['msg' => '¡El Cliente ha sido creado!']);
+        }
+        return response()->json(['msg' => '¡El campo Nombre, Cédula y Fecha son obligatorios!']);
+    }
 }
